e184d51d2d6c70500ac60db4ecb922f59893ddd3
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-07-29 16:11:59 +0300

    modifying corewar.h and rm dublicat defines

--- includes/corewar.h
+++ includes/corewar.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   corewar.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 09:49:51 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/07/29 10:02:10 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/07/29 16:11:28 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef COREWAR_H
 # define COREWAR_H
-# define MEM_SIZE 4096
-# define PLAYERS 4
+
+# include "op.h"
 
 typedef struct		s_arena
 {
@@ -22,17 +22,27 @@ typedef struct		s_arena
 
 typedef struct		s_process
 {
-	char			*p_name;
+	int				id;
+	int				carry;
+	unsigned int	current_opcode;
+	int				cycle_live_reported;
+	int				wait_cycles;
+	int				current_position;
+	int				jump;
+	int				reg[REG_NUMBER];
 	int				cycles;
 	int				live;
+	char			*p_name;
 	t_process		*next;
 }					t_process;
 
 
 typedef struct		s_player
 {
+	int				id;
 	char			*name;
 	char			*comment;
+	int				executable_size;
 	char			*code;
 	int				live;
 	t_process		proc;
@@ -41,9 +51,11 @@ typedef struct		s_player
 typedef struct		s_vm
 {
 	t_arena			a;
-	t_player		p[PLAYERS];
+	t_player		p[MAX_PLAYERS];
+	int				last_live_id;
 	int				cycles;
 	int				current_cycle;
+	int				checks;
 }					t_vm;
 
 #endif
