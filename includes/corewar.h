b9e32694441d7391d48126c5c5ea15cb233dd0b7
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-12 14:36:09 +0300

    cleaning and adding comments into h-file

--- includes/corewar.h
+++ includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/29 09:49:51 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/08/10 18:51:24 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/08/12 14:35:24 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,18 @@ typedef struct		s_arena
 	unsigned char	arena[MEM_SIZE];
 }					t_arena;
 
+/*
+**	Cursors contain following information:
+**	id - unique.
+**	carry - affects zjmp operation, initialised with value false.
+**	current_opcode - operation code, before the battle starts it is not initialised.
+**	cycle_live_reported - number of cycle in which current cursor performed operation live last time.
+**	wait_cycles - amount of cycles to wait before operation execution.
+**	current_position - address in memory
+**	jump - amount of bytes cursor must jump to get to the next operation
+**	reg[REG_NUMBER] - registries of current cursor
+*/
+
 typedef struct		s_process
 {
 	int				id;
@@ -42,6 +54,14 @@ typedef struct		s_process
 	void			*next;
 }					t_process;
 
+/*
+**	Players contain following information:
+**	id - unique ID
+**	name - champion name
+**	comment[COMMENT_LENGTH + 1] - champion comment
+**	executable_size - executable code size
+**	code - executable code
+*/
 
 typedef struct		s_player
 {
@@ -59,6 +79,20 @@ typedef struct		s_player
 	header_t		header;
 	t_process		proc;
 }					t_player;
+
+/*
+**	Game parameters contain following information:
+**	last_live_id - player last reported alive
+**	>It is initialised with the highest player id,
+**	>and is updated every time operation live is performed.
+**	current_cycle - cycles counter
+**	lives - counter for operation live, to check how many times
+**	>this operation was performed during the last cycles_to_die cycles.
+**	ctd - cycles_to_die — length of current check period in cycles.
+**	>This variable is initialised with the value of constant CYCLES_TO_DIE (1536).
+**	dump_cycle - number of cycle to dump memory (if present)
+**	checks - amount of checks performed
+*/
 
 typedef struct		s_vm
 {
