f981707b51a8754db7b51d5c9739aa784f054990
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-18 20:36:51 +0300

    adding tests

--- sources/vm/testing.c
+++ sources/vm/testing.c
@@ -6,31 +6,20 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 10:26:39 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/08/22 11:07:01 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/08/22 11:09:57 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/corewar.h"
 
-static int test1(t_vm *vm)
+static int test2(int i)
 {
-    t_process *current;
-
-    current = vm->proc_list;
-    while(current)
-    {
-        ft_printf("ID: %d\n", current->id);
-        ft_printf("POS: %d\n", current->pc);
-        ft_printf("REG[0]: %d\n", current->reg[0]);
-        ft_printf("\n");
-        current = current->next;
-    }
-    return (1);
+    return (i);
 }
 
-void test(t_vm *vm)
+static int test1(t_vm *vm)
 {
-	t_process *new;
+    t_process *new;
 	new = (t_process*)malloc(sizeof(t_process));
 	new->opcode = 11;
 	new->pc = 0;
@@ -40,4 +29,32 @@ void test(t_vm *vm)
 	// ft_printf("ARG2 %d\n", new->values[1]);
 	// ft_printf("ARG3 %d\n", new->values[2]);
     run_cycles(vm, new);
+    ft_printf("\nCurrent cycle %d\n", vm->current_cycle);
+    return (1);
+}
+
+void test(t_vm *vm)
+{
+    /*
+        Write your tests in this file as a static function
+        which return 1 if test is OK and 0 if test FAILS
+    */
+
+    int testnum = 2; // NUmber of tests : raise this number if you add more tests
+    int i = 0;
+    int tests[testnum];
+
+    // add test row here for invoke your function
+    tests[i++] = test1(vm);
+    tests[i++] = test2(1);
+
+    i = 0;
+    while (i < testnum)
+    {
+        if (tests[i])
+            ft_printf(GREEN "Test %d OK\n" RESET, i);
+        else
+            ft_printf(RED "FAIL %d OK\n" RESET, i);
+        i++;
+    }
 }
