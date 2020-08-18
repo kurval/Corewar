d30eaa774801399a701c902442075a09e981d5b5
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-18 23:03:41 +0300

    adding some comments

--- sources/vm/proc_functions.c
+++ sources/vm/proc_functions.c
@@ -6,7 +6,7 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/18 16:55:01 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/08/22 16:31:23 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/08/22 16:32:35 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,11 +38,18 @@ static void del_node(t_process **proc)
 	*proc = NULL;
 }
 
+/*
+** Check if node is not the first on the list:
+** >point prev to current->next
+** If node is the first on the list:
+** >point the head to current->next
+*/
+
 void    remove_proc(t_process **proc_list, t_process **current, t_process **previous)
 {
-    if (*previous && (*current)->next)
+    if (*previous)
         (*previous)->next = (*current)->next;
-    else
+    else if (*proc_list == *current)
         *proc_list = (*current)->next;
     del_node(current);
     *current = (*previous) ? (*previous)->next : *proc_list;
