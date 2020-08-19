f7786bfb2dd64f87d8acbf222d89c750b17b34aa
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-19 21:45:27 +0300

    adding operations: add and sub

--- sources/vm/operations/op_zjmp.c
+++ sources/vm/operations/op_zjmp.c
@@ -6,11 +6,16 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/08 11:46:09 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/08/19 16:57:19 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/08/19 21:43:17 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/corewar.h"
+
+/*
+** if carry == 1 move cursor to:
+** current_position + (T_DIR % IDX_MOD)							
+*/
 
 void    op_zjmp(t_vm *vm, t_process *proc)
 {
