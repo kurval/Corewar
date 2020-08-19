4469e0cb0cac97d9617b6ca950c2050d50bdabdd
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-19 22:15:45 +0300

    adding operations and, or, xor

--- sources/vm/operations/op_or.c
+++ sources/vm/operations/op_or.c
@@ -6,15 +6,26 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/08 11:45:46 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/08/14 21:05:45 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/08/19 22:14:47 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../includes/corewar.h"
 
+/*
+** Apply bit operation |
+** between ARG1 value and ARG1 value
+** and save result to reg[ARG3]
+** if result == 0 set carry to 1 else 0 
+*/
+
 void    op_or(t_vm *vm, t_process *proc)
 {
-    if (proc == NULL)
-        ;
-    ft_printf("function or !%d\n", vm->lives);
+    int res;
+
+    res = proc->values[0] | proc->values[1];
+    proc->reg[proc->values[2] - 1] = res;
+    proc->carry = (res == 0) ? 1 : 0;
+    if (vm)
+        return ;
 }
