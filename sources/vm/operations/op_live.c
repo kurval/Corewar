7f954af9cd2fa8dbcb3053f0ec58037a4e80a1d4
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-08-20 15:38:58 +0300

    final fix to live

--- sources/vm/operations/op_live.c
+++ sources/vm/operations/op_live.c
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/08 11:45:38 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/08/20 14:18:41 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/08/20 15:38:31 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,8 @@ void	op_live(t_vm *vm, t_process *proc)
 
 	vm->lives++;
 	proc->last_live = vm->current_cycle;
-	value1 = -1 * get_op_values(vm, proc, 1);
-	if (proc->reg[0] == value1)
+	value1 = get_op_values(vm, proc, 1);
+	if (proc->reg[0] == value1 * -1)
 		vm->last_live_id = value1;
 	ft_printf("A process shows that player %d (%s) is alive\n",
 	((t_player *)proc->player)->id, ((t_player *)proc->player)->name);
