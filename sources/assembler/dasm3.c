1f5340d36c5e5243e7e278a5d4e06996a87e7a8b
Besnik <besnik.konjuhaj@hotmail.com>
2020-09-14 08:21:24 +0300

    improved disassembler

--- sources/assembler/dasm3.c
+++ sources/assembler/dasm3.c
@@ -6,7 +6,7 @@
 /*   By: bkonjuha <bkonjuha@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/12 15:22:12 by bkonjuha          #+#    #+#             */
-/*   Updated: 2020/09/13 10:50:43 by bkonjuha         ###   ########.fr       */
+/*   Updated: 2020/09/14 08:20:01 by bkonjuha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		read_n_bytes(int input, int count)
 {
 	unsigned char	number;
-	signed char		result;
+	int				result;
 
 	number = 0;
 	result = 0;
@@ -30,7 +30,7 @@ int		read_n_bytes(int input, int count)
 
 void	write_t_reg(int output, int input)
 {
-	int number;
+	char number;
 
 	number = 0;
 	write(output, " r", 2);
@@ -40,20 +40,27 @@ void	write_t_reg(int output, int input)
 
 void	write_t_dir(int output, int input, int ins)
 {
-	int number;
+	int		number;
+	short	s_num;
 
 	write(output, " %", 2);
 	number = 0;
+	s_num = 0;
 	if (ins > 8 && ins < 17 && ins != 13)
-		number = read_n_bytes(input, 2);
+	{
+		s_num = read_n_bytes(input, 2);
+		ft_putnbr_fd(s_num, output);
+	}
 	else
+	{
 		number = read_n_bytes(input, 4);
-	ft_putnbr_fd(number, output);
+		ft_putnbr_fd(number, output);
+	}
 }
 
 void	write_t_ind(int output, int input)
 {
-	int number;
+	short number;
 
 	number = 0;
 	number = read_n_bytes(input, 2);
