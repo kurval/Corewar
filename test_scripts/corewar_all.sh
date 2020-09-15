adf94507fd1b8f6ac6758aff3e774f6b091899ff
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-09-15 15:19:59 +0300

    adding asm manpace back and couple of corewar tests

--- test_scripts/corewar_all.sh
+++ test_scripts/corewar_all.sh
@@ -13,3 +13,18 @@ read -p "PRESS ENTER TO CONTINUE"
 read -p "PRESS ENTER TO CONTINUE"
 ./cw_test.sh --dump 1000 cor_files/Rainbow_dash.cor cor_files/_.cor
 read -p "PRESS ENTER TO CONTINUE"
+
+./cw_test.sh --test cor_files/ex.cor cor_files/turtle.cor cor_files/helltrain.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
+./cw_test.sh --dump 17030 cor_files/ex.cor cor_files/turtle.cor cor_files/helltrain.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
+
+./cw_test.sh --test cor_files/turtle.cor cor_files/helltrain.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
+./cw_test.sh --dump 10000 cor_files/turtle.cor cor_files/helltrain.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
+
+./cw_test.sh --test cor_files/turtle.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
+./cw_test.sh --dump 12000 cor_files/turtle.cor cor_files/fluttershy.cor
+read -p "PRESS ENTER TO CONTINUE"
