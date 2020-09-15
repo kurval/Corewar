750e54c52c95d34e0669b87916d84c460f7a8a53
janettem <janette.metelinen@gmail.com>
2020-09-15 08:33:54 +0100

    Fixed a norm error in corewar.h

--- includes/corewar.h
+++ includes/corewar.h
@@ -270,7 +270,7 @@ void				print_player_info(t_vm *vm);
 void				init_visualizer(t_vm *vm);
 void				manage_windows(t_vm *vm, int key);
 void				draw_log(t_vm *vm);
-void    			log_operation(t_vm *vm, t_process *proc, int i);
+void				log_operation(t_vm *vm, t_process *proc, int i);
 void				print_log_text(t_vm *vm, t_process *proc, char *str,
 					int color_num);
 void				set_debug(t_vm *vm);
