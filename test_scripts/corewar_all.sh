b3bad279778f462e72f22db05e8bb841c3d25fda
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-09-17 10:26:07 +0300

    adding test to corewar test. rm test.sh/no longer needed

--- test_scripts/corewar_all.sh
+++ test_scripts/corewar_all.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+./cw_test.sh --test cor_files/jinx.cor cor_files/dubo.cor cor_files/big_feet.cor cor_files/overwatch.cor
+read -p "PRESS ENTER TO CONTINUE"
+./cw_test.sh --dump 1 cor_files/jinx.cor cor_files/dubo.cor cor_files/big_feet.cor cor_files/overwatch.cor
+read -p "PRESS ENTER TO CONTINUE"
+
 ./cw_test.sh --test cor_files/Wall.cor cor_files/Eraser.cor cor_files/mortel.cor cor_files/NoIdea.cor
 read -p "PRESS ENTER TO CONTINUE"
 ./cw_test.sh --dump 1000 cor_files/Wall.cor cor_files/Eraser.cor cor_files/mortel.cor cor_files/NoIdea.cor
