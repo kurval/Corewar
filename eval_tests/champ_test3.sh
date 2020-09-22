20e7d849038e91c1104722f9e968db25c22547b7
janettem <janette.metelinen@gmail.com>
2020-09-22 12:45:13 +0100

    Added another script to test champs

---
+++ eval_tests/champ_test3.sh
@@ -0,0 +1,47 @@
+../corewar $1 $2 $3 $4
+echo
+../corewar $1 $2 $4 $3
+echo
+../corewar $1 $3 $2 $4
+echo
+../corewar $1 $3 $4 $2
+echo
+../corewar $1 $4 $2 $3
+echo
+../corewar $1 $4 $3 $2
+echo
+../corewar $2 $1 $3 $4
+echo
+../corewar $2 $1 $4 $3
+echo
+../corewar $2 $3 $1 $4
+echo
+../corewar $2 $3 $4 $1
+echo
+../corewar $2 $4 $1 $3
+echo
+../corewar $2 $4 $3 $1
+echo
+../corewar $3 $1 $2 $4
+echo
+../corewar $3 $1 $4 $2
+echo
+../corewar $3 $2 $1 $4
+echo
+../corewar $3 $2 $4 $1
+echo
+../corewar $3 $4 $1 $2
+echo
+../corewar $3 $4 $2 $1
+echo
+../corewar $4 $1 $2 $3
+echo
+../corewar $4 $1 $3 $2
+echo
+../corewar $4 $2 $1 $3
+echo
+../corewar $4 $2 $3 $1
+echo
+../corewar $4 $3 $1 $2
+echo
+../corewar $4 $3 $2 $1
